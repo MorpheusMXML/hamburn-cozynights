1b2c7cc1f874f17cb4a4936a307fb6a18ce112c5
Maximilian Brosius <brosiusmax@gmail.com>
2025-10-26 22:10:34 +0100

    change of plans :D SUpabase and Relational it is.

--- src/lib/server/crypto.ts
+++
@@ -1,9 +0,0 @@
-import bcrypt from 'bcryptjs';
-
-export async function hashCode(code: string) {
-  const salt = await bcrypt.genSalt(10);
-  return bcrypt.hash(code, salt);
-}
-export async function verifyCode(code: string, codeHash: string) {
-  return bcrypt.compare(code, codeHash);
-}
