9ad54084a4baae9cd3372a8addccc4ff24088373
Maximilian Brosius <brosiusmax@gmail.com>
2026-01-21 18:24:57 +0100

    fixed map page and redirect with correct order number

--- hamburn-cozynights/src/lib/pocketbase.ts
+++ hamburn-cozynights/src/lib/pocketbase.ts
@@ -1,10 +1,13 @@
+// src/lib/pocketbase.ts
 import PocketBase from 'pocketbase';
+import { browser } from '$app/environment'; // Importiere browser check
 
-// Ersetze die URL ggf. mit deiner Produktions-URL, falls du nicht lokal arbeitest
 export const pb = new PocketBase('http://127.0.0.1:8090');
 
-// Optional: Auth-Store automatisch laden/speichern (für Logins wichtig)
- pb.authStore.loadFromCookie(document.cookie);
-pb.authStore.onChange(() => {
-   document.cookie = pb.authStore.exportToCookie({ httpOnly: false });
-});
+// Nur im Browser ausführen
+if (browser) {
+    pb.authStore.loadFromCookie(document.cookie);
+    pb.authStore.onChange(() => {
+       document.cookie = pb.authStore.exportToCookie({ httpOnly: false });
+    });
+}
