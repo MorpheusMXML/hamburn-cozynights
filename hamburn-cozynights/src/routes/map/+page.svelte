e3afca778d32532a1d87ca9ab3ccce68f129f67a
Maximilian Brosius <brosiusmax@gmail.com>
2026-02-06 13:13:19 +0100

    Map Placement improved still not perfect. Reloading fixed

--- hamburn-cozynights/src/routes/map/+page.svelte
+++ hamburn-cozynights/src/routes/map/+page.svelte
@@ -1,19 +1,43 @@
 <script lang="ts">
-  import Map from '$lib/components/Map.svelte';
   import type { PageData } from './$types';
+  import Map from '$lib/components/Map.svelte';
+  import { onMount } from 'svelte';
+  import { invalidateAll } from '$app/navigation';
 
   export let data: PageData;
+
+  // Reaktivität sicherstellen
+  $: ({ houses } = data);
+
+  onMount(() => {
+    // Erzwingt beim Betreten der Seite, dass die Load-Funktion 
+    // vom Server (page.server.ts) erneut aufgerufen wird.
+    invalidateAll();
+  });
 </script>
 
-<div class="map-wrapper">
-  <Map houses={data.houses} />
+<div class="page-container">
+  {#if houses}
+    {#key houses}
+      <Map {houses} isEditorMode={false} />
+    {/key}
+  {:else}
+    <div class="loading">Loading Map...</div>
+  {/if}
 </div>
 
 <style>
-  .map-wrapper {
+  .page-container {
     width: 100vw;
     height: 100vh;
-    overflow: hidden;
     background: #050505;
+    overflow: hidden;
+  }
+  .loading {
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
   }
 </style>
